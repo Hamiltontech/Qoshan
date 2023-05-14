dcd1cb64a02aeff89cfee1bbe32b3850f958c292
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-05-14 23:11:05 +0300

    Header Modification

--- src/components/home-8/Header.jsx
+++ src/components/home-8/Header.jsx
@@ -18,57 +18,47 @@ const Header = () => {
   }, []);
 
   return (
-    
-    <header
-      className={`header-nav menu_style_home_one style2 home8  navbar-scrolltofixed stricky main-menu rtl ${
-        navbar ? "stricky-fixed " : ""
-      }`}
-    >
-      <div className="container-fluid p-0">
-        {/* site logo brand */}
-        <div className="d-flex align-items-center justify-content-between px-3">
-          <div className="d-flex align-items-center">
-            <Link href="/">
-              <a className="me-3">
-                <img
-                  className="logo1 img-fluid"
-                  src="assets/images/header-logo2.svg"
-                  alt="header-logo2.svg"
-                  style={{ height: 80 }}
-                />
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="position-relative" style={{ zIndex: 1000, marginBottom: -130, paddingLeft: 40, paddingRight: 40,}}>
-                <img
-                  className="logo img-fluid winner"
-                  src="assets/images/winner.webp"
-                  alt="header-logo2.svg"
-                  style={{ height: 200}}
-                />
-              </a>
-            </Link>
+    <div className="top-header d-flex align-items-center justify-content-between py-2 px-3" style={{ backgroundColor: "#232323" }}>
+      
+      <header
+        className={`header-nav menu_style_home_one style2 home8  navbar-scrolltofixed stricky main-menu rtl ${
+          navbar ? "stricky-fixed " : ""
+          }`}
+      >
+        
+        <div className="container-fluid p-0">
+          {/* site logo brand */}
+          <div className="d-flex align-items-center justify-content-between">
+            <div className="d-flex align-items-center">
+              <Link href="/">
+                <a className="me-3">
+                  <img
+                    className="logo1 img-fluid"
+                    src="assets/images/header-logo2.svg"
+                    alt="header-logo2.svg"
+                    style={{ height: 80 }}
+                  />
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="position-relative" style={{ zIndex: 1000, marginBottom: -60, paddingLeft: 40, paddingRight: 40 }}>
+                  <img
+                    className="logo img-fluid winner"
+                    src="assets/images/winner.webp"
+                    alt="header-logo2.svg"
+                    style={{ height: 150 }}
+                  />
+                </a>
+              </Link>
+            </div>
+            <nav className="d-flex align-items-center justify-content-center" style={{ flex: 1 }}>
+              <HeaderMenuContent />
+            </nav>
           </div>
-          <nav className="d-flex align-items-center justify-content-center" style={{ flex: 1 }}>
-            <HeaderMenuContent />
-          </nav>
-          <div className="d-flex align-items-center" style={{flexDirection: "column",}}>
-  <div className="social-icons me-3">
-    <a href="#"><i className="fa fa-facebook" style={{fontSize: '18px', color: '#c2b49a', marginRight: '10px'}}></i></a>
-    <a href="#"><i className="fa fa-instagram" style={{fontSize: '18px', color: '#c2b49a', marginRight: '10px'}}></i></a>
-    <a href="#"><i className="fa fa-twitter" style={{fontSize: '18px', color: '#c2b49a', marginRight: '10px'}}></i></a>
-  </div>
-  <div className="phone-number">
-    <a href="#"><i className="fa fa-phone" style={{fontSize: '18px', color: '#c2b49a', marginRight: '10px'}}></i> </a>
-    <span className="ms-2" style={{color: '#c2b49a'}}>0791929666</span>
-  </div>
-</div>
-
-
+          {/* End .navbar */}
         </div>
-        {/* End .navbar */}
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
