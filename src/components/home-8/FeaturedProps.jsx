181b13b684ea7b4d1e3e3fdf0f5440d69c7a4598
Diala Abul-Khail <97671741+dialaabulkhail@users.noreply.github.com>
2023-05-15 18:53:19 +0000

    featured

--- src/components/home-8/FeaturedProps.jsx
+++ src/components/home-8/FeaturedProps.jsx
@@ -60,9 +60,9 @@ const FeaturedProperties = () => {
   <div className="item" key={item?.id} dir="rtl">
     <div className="feat_property home3">
       <div className="thumb">
-        {/* <img className="img-whp" src={'https://strapi-125841-0.cloudclusters.net' + item?.attributes?.Featured?.data?.attributes?.formats?.large?.url} alt="fp1.jpg" /> */}
+        <img className="img-whp" src={'https://strapi-125841-0.cloudclusters.net' + item?.attributes?.Featured?.data?.attributes?.formats?.large?.url} alt="fp1.jpg" />
         <div className="thmb_cntnt">
-          {properties.map((it)=>(
+          {/* {properties.map((it)=>(
             <ul className="tag mb0">
             {it.saleTag.map((val, i) => (
               <li className="list-inline-item" key={i}>
@@ -70,14 +70,14 @@ const FeaturedProperties = () => {
               </li>
             ))}
           </ul>
-            ))}
+            ))} */}
           
 
           
 
           <Link href={`/listing-details-v1/${item?.id}`}>
             <a className="fp_price">
-            {item?.attributes?.Price?.slice(0,3)},{item?.attributes?.Price?.slice(3)} دينار أردني
+            {/* {item?.attributes?.Price?.slice(0,3)},{item?.attributes?.Price?.slice(3)} دينار أردني */}
 
             </a>
           </Link>
