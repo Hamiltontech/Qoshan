48f740d9003f08fa2e451ea29b9fbf68b5e63624
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-05-19 23:36:36 +0300

    Footer Added

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -5,44 +5,101 @@ import SubscribeForm from "./SubscribeForm";
 const Footer = () => {
   return (
     <>
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 pr0 pl0">
-        <div className="footer_about_widget">
-          <h4>About Site</h4>
-          <p>
-            We’re reimagining how you buy, sell and rent. It’s now easier to get
-            into a place you love. So let’s do this, together.
-          </p>
-        </div>
-      </div>
+   
       {/* End .col */}
 
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
         <div className="footer_qlink_widget">
-          <h4>Quick Links</h4>
           <ul className="list-unstyled">
             <li>
               <Link href="/">
-                <a>About Us</a>
+                <a>فلل مستقلة في دابوق للبيع</a>
               </Link>
             </li>
             <li>
               <Link href="/">
-                <a>Terms & Conditions</a>
+                <a>شقق أرضية في عبدون للبيع</a>
               </Link>
             </li>
             <li>
               <Link href="/">
-                <a>User’s Guide</a>
+                <a>رووف مع تراس بإطلالة مميزة </a>
               </Link>
             </li>
             <li>
               <Link href="/">
-                <a>Support Center</a>
+                <a>شقق طابقية في أم أذنية للبيع</a>
               </Link>
             </li>
             <li>
               <Link href="/">
-                <a>Press Info</a>
+                <a>شقق دوبلكيس في عبدون للبيع</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* End .col */}
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
+        <div className="footer_qlink_widget">
+          <ul className="list-unstyled">
+            <li>
+              <Link href="/">
+                <a>فلل مستقلة في دابوق للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق أرضية في عبدون للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>رووف مع تراس بإطلالة مميزة </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق طابقية في أم أذنية للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق دوبلكيس في عبدون للبيع</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+      {/* End .col */}
+
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
+        <div className="footer_qlink_widget">
+          <ul className="list-unstyled">
+            <li>
+              <Link href="/">
+                <a>فلل مستقلة في دابوق للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق أرضية في عبدون للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>رووف مع تراس بإطلالة مميزة </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق طابقية في أم أذنية للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق دوبلكيس في عبدون للبيع</a>
               </Link>
             </li>
           </ul>
@@ -50,38 +107,65 @@ const Footer = () => {
       </div>
       {/* End .col */}
 
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+      <div className="col-sm-4 col-md-6 col-lg-3 col-xl-3 p-10">
+        <div className="footer_qlink_widget">
+          <ul className="list-unstyled">
+            <li>
+              <Link href="/">
+                <a>فلل مستقلة في دابوق للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق أرضية في عبدون للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>رووف مع تراس بإطلالة مميزة </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق طابقية في أم أذنية للبيع</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>شقق دوبلكيس في عبدون للبيع</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-6" style={{marginTop: 20}}>
         <div className="footer_contact_widget">
-          <h4>Contact Us</h4>
+          <h4>تواصل معنا</h4>
           <ul className="list-unstyled">
             <li>
-              <a href="mailto:info@findhouse.com">info@findhouse.com</a>
+              <a href="mailto:info@findhouse.com">info@qoshan.com</a>
             </li>
             <li>
-              <a href="#">Collins Street West, Victoria</a>
+              <a href="#">العنوان :الدوار السابع ، مجمع الشروقي</a>
             </li>
+            
             <li>
-              <a href="#">8007, Australia.</a>
+              <a href="tel:+962-796-868-500">+962-796-868-500</a>
             </li>
-            <li>
-              <a href="tel:+4733378901">+1 246-345-0699</a>
-            </li>
-            <li>
-              <a href="tel:+4733378901">+1 246-345-0695</a>
-            </li>
+            
           </ul>
         </div>
       </div>
       {/* End .col */}
 
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-6" style={{marginTop: 20}}> 
         <div className="footer_social_widget">
-          <h4>Follow us</h4>
+          <h4>تابعنا</h4>
           <ul className="mb30">
             <Social />
           </ul>
-          <h4>Subscribe</h4>
-          <SubscribeForm />
         </div>
       </div>
     </>
