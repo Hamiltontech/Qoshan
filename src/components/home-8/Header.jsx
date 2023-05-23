5549a111857e1f19135be76ed4e8179fd558eb34
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-05-24 02:49:08 +0300

    404 page fix

--- src/components/home-8/Header.jsx
+++ src/components/home-8/Header.jsx
@@ -4,7 +4,7 @@ import HeaderMenuContent from "../common/header/HeaderMenuContent";
 import WhatsAppButton from "./whatsapp";
 import axios from "axios";
 import Image from "next/image";
-
+import Social from "./social";
 const Header = () => {
   const [navbar, setNavbar] = useState(false);
   const [data, setData] = useState({});
@@ -33,19 +33,9 @@ const Header = () => {
   return (
     <div className="top-header d-flex align-items-center justify-content-between py-2 px-3" style={{ backgroundColor: "#232323" }}>
       <WhatsAppButton />
-      <div className="social-media-links" style={{ backgroundColor: "#1f1f1f", padding: "10px 0", position: "absolute", zIndex: 2, top: 0, left: 0, right: 0, display: "none" }}>
-        {/* Add your social media links here */}
-        {/* Example: */}
-        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-          Facebook
-        </a>
-        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-          Twitter
-        </a>
-        {/* Add more social media links as needed */}
-      </div>
-
       <header className={`header-nav menu_style_home_one style2 home8  navbar-scrolltofixed stricky main-menu rtl ${navbar ? "stricky-fixed " : ""}`} style={{ display: "block" }}>
+      <Social/>
+
         <div className="container-fluid p-0">
           {/* site logo brand */}
           <div className="d-flex align-items-center justify-content-between">
