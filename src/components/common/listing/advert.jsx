94a07f8cb114f4f8e1408c06b42d0360b6212d44
Hamilton Tech <118741438+Hamiltontech@users.noreply.github.com>
2023-05-24 01:14:21 +0300

    advert

--- src/components/common/listing/advert.jsx
+++ src/components/common/listing/advert.jsx
@@ -7,7 +7,7 @@ const FindProperties = () => {
     const [data, setData] = useState({});
     useEffect(() => {
         axios
-          .get("https://strapi-125841-0.cloudclusters.net/api/under-search-advert?populate=*")
+          .get("https://strapi-125841-0.cloudclusters.net/api/side-advert?populate=*")
           .then((response) => {
             const res = response.data.data;
             setData(res);
@@ -23,8 +23,8 @@ const FindProperties = () => {
                 <a className="position-relative" style={{ marginRight: 15, marginLeft: 15 }}>
                   {/* <img className="logo img-fluid winner" src="https://res.cloudinary.com/dhk7qsnfv/image/upload/v1684257815/winner-land_jm56zk.webp" alt="header-logo2.svg" style={{ height: 80 }} /> */}
                   <Image src={'https://strapi-125841-0.cloudclusters.net' + data?.attributes?.advert?.data?.attributes?.url}
-                      width={729}
-                      height={90}
+                      width={616}
+                      height={1023}
                       />                                </a>
               </a>
     </>
