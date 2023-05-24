e93bc4aa096bf6e6ad0c76c80ce65a7579c53953
Diala Abul-Khail <97671741+dialaabulkhail@users.noreply.github.com>
2023-05-24 06:29:12 +0000

    footer links

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,168 +1,179 @@
 import Link from "next/link";
+
 import Social from "./Social";
+
 import SubscribeForm from "./SubscribeForm";
 
 const Footer = () => {
   return (
     <>
+       
+
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
+        <div className="footer_qlink_widget">
+          <ul className="list-unstyled">
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D9%81%D9%84%D9%84+%D9%85%D8%B3%D8%AA%D9%82%D9%84%D8%A9+%D9%81%D9%8A+%D8%AF%D8%A7%D8%A8%D9%88%D9%82+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>فلل مستقلة في دابوق للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A3%D8%B1%D8%B6%D9%8A%D8%A9+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق أرضية في عبدون للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B1%D9%88%D9%88%D9%81+%D9%85%D8%B9+%D8%AA%D8%B1%D8%A7%D8%B3+%D8%A8%D8%A5%D8%B7%D9%84%D8%A7%D9%84%D8%A9+%D9%85%D9%85%D9%8A%D8%B2%D8%A9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>رووف مع تراس بإطلالة مميزة </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%B7%D8%A7%D8%A8%D9%82%D9%8A%D8%A9+%D9%81%D9%8A+%D8%A3%D9%85+%D8%A3%D8%B0%D9%86%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق طابقية في أم أذنية للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%AF%D9%88%D8%A8%D9%84%D9%83%D9%8A%D8%B3+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق دوبلكيس في عبدون للبيع</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+       
+
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
+        <div className="footer_qlink_widget">
+          <ul className="list-unstyled">
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D9%82%D8%B7%D8%B9+%D8%A3%D8%B1%D8%A7%D8%B6%D9%8A+%D8%B3%D9%83%D9%86%D9%8A%D8%A9+%D8%B9%D9%84%D9%89+%D8%B7%D8%B1%D9%8A%D9%82+%D8%A7%D9%84%D9%85%D8%B7%D8%A7%D8%B1+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>قطع أراضي سكنية على طريق المطار للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%82%D9%8A%D8%AF+%D8%A7%D9%84%D8%A5%D9%86%D8%B4%D8%A7%D8%A1+%D9%81%D9%8A+%D8%B9%D9%85%D8%A7%D9%86+%D8%A7%D9%84%D8%BA%D8%B1%D8%A8%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق قيد الإنشاء في عمان الغربية للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A3%D8%B1%D8%B6%D9%8A%D8%A9+%D9%85%D8%B9+%D8%AD%D8%AF%D9%8A%D9%82%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق أرضية مع حديقة للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%B7%D8%A7%D8%A8%D9%82%D9%8A%D8%A9+%D9%81%D9%8A+%D8%A3%D9%85+%D8%A3%D8%B0%D9%86%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق طابقية في أم أذنية للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D9%81%D9%84%D9%84+%D9%85%D8%AA%D9%84%D8%A7%D8%B5%D9%82%D8%A9+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>فلل متلاصقة في عبدون للبيع</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
+        <div className="footer_qlink_widget">
+          <ul className="list-unstyled">
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%B0%D8%A7%D8%AA+%D8%AC%D9%88%D8%AF%D8%A9+%D8%B9%D8%A7%D9%84%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق ذات جودة عالية للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A3%D8%B1%D8%B6%D9%8A%D8%A9+%D8%AF%D9%88%D8%A8%D9%84%D9%83%D8%B3+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق أرضية دوبلكس للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%81%D9%8A+%D9%85%D8%AC%D9%85%D8%B9%D8%A7%D8%AA+%D8%B3%D9%83%D9%86%D9%8A%D8%A9+%D9%85%D8%B9+%D8%AE%D8%AF%D9%85%D8%A7%D8%AA+%D9%85%D9%85%D9%8A%D8%B2%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق في مجمعات سكنية مع خدمات مميزة للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%85%D9%81%D8%B1%D9%88%D8%B4%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق مفروشة للبيع</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%81%D9%8A+%D8%AE%D9%84%D8%AF%D8%A7+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق في خلدا للبيع</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
    
-      {/* End .col */}
-
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
-        <div className="footer_qlink_widget">
-          <ul className="list-unstyled">
-            <li>
-              <Link href="/">
-                <a>فلل مستقلة في دابوق للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق أرضية في عبدون للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>رووف مع تراس بإطلالة مميزة </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق طابقية في أم أذنية للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق دوبلكيس في عبدون للبيع</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      {/* End .col */}
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
-        <div className="footer_qlink_widget">
-          <ul className="list-unstyled">
-            <li>
-              <Link href="/">
-                <a>فلل مستقلة في دابوق للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق أرضية في عبدون للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>رووف مع تراس بإطلالة مميزة </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق طابقية في أم أذنية للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق دوبلكيس في عبدون للبيع</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      
-      {/* End .col */}
-
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-10">
-        <div className="footer_qlink_widget">
-          <ul className="list-unstyled">
-            <li>
-              <Link href="/">
-                <a>فلل مستقلة في دابوق للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق أرضية في عبدون للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>رووف مع تراس بإطلالة مميزة </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق طابقية في أم أذنية للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق دوبلكيس في عبدون للبيع</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      {/* End .col */}
 
       <div className="col-sm-4 col-md-6 col-lg-3 col-xl-3 p-10">
         <div className="footer_qlink_widget">
           <ul className="list-unstyled">
             <li>
-              <Link href="/">
-                <a>فلل مستقلة في دابوق للبيع</a>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%A7%D8%B3%D8%AA%D8%AF%D9%8A%D9%88%D9%87%D8%A7%D8%AA+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>استديوهات في عبدون للبيع</a>
               </Link>
             </li>
+
             <li>
-              <Link href="/">
-                <a>شقق أرضية في عبدون للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>رووف مع تراس بإطلالة مميزة </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق طابقية في أم أذنية للبيع</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>شقق دوبلكيس في عبدون للبيع</a>
+              <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A8%D8%AA%D8%B4%D8%B7%D9%8A%D8%A8%D8%A7%D8%AA+%D9%85%D9%85%D9%8A%D8%B2%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
+                <a>شقق بتشطيبات مميزة للبيع</a>
               </Link>
             </li>
           </ul>
         </div>
       </div>
-      {/* End .col */}
 
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-6" style={{marginTop: 20}}>
+      
+
+      <div
+        className="col-sm-6 col-md-6 col-lg-3 col-xl-6"
+        style={{ marginTop: 20 }}
+      >
         <div className="footer_contact_widget">
           <h4>تواصل معنا</h4>
+
           <ul className="list-unstyled">
             <li>
               <a href="mailto:info@findhouse.com">info@qoshan.com</a>
             </li>
+
             <li>
               <a href="#">العنوان :الدوار السابع ، مجمع الشروقي</a>
             </li>
-            
+
             <li>
               <a href="tel:+962-796-868-500">+962-796-868-500</a>
             </li>
-            
           </ul>
         </div>
       </div>
-      {/* End .col */}
 
-      <div className="col-sm-6 col-md-6 col-lg-3 col-xl-6" style={{marginTop: 20}}> 
+
+      <div
+        className="col-sm-6 col-md-6 col-lg-3 col-xl-6"
+        style={{ marginTop: 20 }}
+      >
         <div className="footer_social_widget">
           <h4>تابعنا</h4>
+
           <ul className="mb30">
             <Social />
           </ul>
