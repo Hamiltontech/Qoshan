c3510329c379ed834c98b988851cc2f8811c6bf0
Diala Abul-Khail <97671741+dialaabulkhail@users.noreply.github.com>
2023-05-24 09:08:39 +0000

    restores

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -14,31 +14,31 @@ const Footer = () => {
           <ul className="list-unstyled">
             <li>
               <Link  href="https://qoshan.com/all-properties?keyword=%D9%81%D9%84%D9%84+%D9%85%D8%B3%D8%AA%D9%82%D9%84%D8%A9+%D9%81%D9%8A+%D8%AF%D8%A7%D8%A8%D9%88%D9%82+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">فلل مستقلة في دابوق للبيع</a>
+                <a target="_blank" rel="noreferrer">فلل مستقلة في دابوق للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A3%D8%B1%D8%B6%D9%8A%D8%A9+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق أرضية في عبدون للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق أرضية في عبدون للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B1%D9%88%D9%88%D9%81+%D9%85%D8%B9+%D8%AA%D8%B1%D8%A7%D8%B3+%D8%A8%D8%A5%D8%B7%D9%84%D8%A7%D9%84%D8%A9+%D9%85%D9%85%D9%8A%D8%B2%D8%A9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">رووف مع تراس بإطلالة مميزة </a>
+                <a target="_blank" rel="noreferrer">رووف مع تراس بإطلالة مميزة </a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%B7%D8%A7%D8%A8%D9%82%D9%8A%D8%A9+%D9%81%D9%8A+%D8%A3%D9%85+%D8%A3%D8%B0%D9%86%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق طابقية في أم أذنية للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق طابقية في أم أذنية للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%AF%D9%88%D8%A8%D9%84%D9%83%D9%8A%D8%B3+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق دوبلكيس في عبدون للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق دوبلكيس في عبدون للبيع</a>
               </Link>
             </li>
           </ul>
@@ -52,31 +52,31 @@ const Footer = () => {
           <ul className="list-unstyled">
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D9%82%D8%B7%D8%B9+%D8%A3%D8%B1%D8%A7%D8%B6%D9%8A+%D8%B3%D9%83%D9%86%D9%8A%D8%A9+%D8%B9%D9%84%D9%89+%D8%B7%D8%B1%D9%8A%D9%82+%D8%A7%D9%84%D9%85%D8%B7%D8%A7%D8%B1+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">قطع أراضي سكنية على طريق المطار للبيع</a>
+                <a target="_blank" rel="noreferrer">قطع أراضي سكنية على طريق المطار للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%82%D9%8A%D8%AF+%D8%A7%D9%84%D8%A5%D9%86%D8%B4%D8%A7%D8%A1+%D9%81%D9%8A+%D8%B9%D9%85%D8%A7%D9%86+%D8%A7%D9%84%D8%BA%D8%B1%D8%A8%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق قيد الإنشاء في عمان الغربية للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق قيد الإنشاء في عمان الغربية للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A3%D8%B1%D8%B6%D9%8A%D8%A9+%D9%85%D8%B9+%D8%AD%D8%AF%D9%8A%D9%82%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق أرضية مع حديقة للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق أرضية مع حديقة للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%B7%D8%A7%D8%A8%D9%82%D9%8A%D8%A9+%D9%81%D9%8A+%D8%A3%D9%85+%D8%A3%D8%B0%D9%86%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق طابقية في أم أذنية للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق طابقية في أم أذنية للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D9%81%D9%84%D9%84+%D9%85%D8%AA%D9%84%D8%A7%D8%B5%D9%82%D8%A9+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">فلل متلاصقة في عبدون للبيع</a>
+                <a target="_blank" rel="noreferrer">فلل متلاصقة في عبدون للبيع</a>
               </Link>
             </li>
           </ul>
@@ -90,31 +90,31 @@ const Footer = () => {
           <ul className="list-unstyled">
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%B0%D8%A7%D8%AA+%D8%AC%D9%88%D8%AF%D8%A9+%D8%B9%D8%A7%D9%84%D9%8A%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق ذات جودة عالية للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق ذات جودة عالية للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A3%D8%B1%D8%B6%D9%8A%D8%A9+%D8%AF%D9%88%D8%A8%D9%84%D9%83%D8%B3+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق أرضية دوبلكس للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق أرضية دوبلكس للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%81%D9%8A+%D9%85%D8%AC%D9%85%D8%B9%D8%A7%D8%AA+%D8%B3%D9%83%D9%86%D9%8A%D8%A9+%D9%85%D8%B9+%D8%AE%D8%AF%D9%85%D8%A7%D8%AA+%D9%85%D9%85%D9%8A%D8%B2%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق في مجمعات سكنية مع خدمات مميزة للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق في مجمعات سكنية مع خدمات مميزة للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%85%D9%81%D8%B1%D9%88%D8%B4%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق مفروشة للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق مفروشة للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D9%81%D9%8A+%D8%AE%D9%84%D8%AF%D8%A7+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق في خلدا للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق في خلدا للبيع</a>
               </Link>
             </li>
           </ul>
@@ -128,13 +128,13 @@ const Footer = () => {
           <ul className="list-unstyled">
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%A7%D8%B3%D8%AA%D8%AF%D9%8A%D9%88%D9%87%D8%A7%D8%AA+%D9%81%D9%8A+%D8%B9%D8%A8%D8%AF%D9%88%D9%86+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">استديوهات في عبدون للبيع</a>
+                <a target="_blank" rel="noreferrer">استديوهات في عبدون للبيع</a>
               </Link>
             </li>
 
             <li>
               <Link href="https://qoshan.com/all-properties?keyword=%D8%B4%D9%82%D9%82+%D8%A8%D8%AA%D8%B4%D8%B7%D9%8A%D8%A8%D8%A7%D8%AA+%D9%85%D9%85%D9%8A%D8%B2%D8%A9+%D9%84%D9%84%D8%A8%D9%8A%D8%B9&area=&type=all&minPrice=%D8%A3%D9%82%D9%84+%D8%B3%D8%B9%D8%B1&maxPrice=%D8%A3%D8%B9%D9%84%D9%89+%D8%B3%D8%B9%D8%B1/">
-                <a target="_blank">شقق بتشطيبات مميزة للبيع</a>
+                <a target="_blank" rel="noreferrer">شقق بتشطيبات مميزة للبيع</a>
               </Link>
             </li>
           </ul>
@@ -152,15 +152,15 @@ const Footer = () => {
 
           <ul className="list-unstyled">
             <li>
-              <a target="_blank" href="mailto:info@findhouse.com">info@qoshan.com</a>
+              <a target="_blank" rel="noreferrer" href="mailto:info@findhouse.com">info@qoshan.com</a>
             </li>
 
             <li>
-              <a target="_blank" href="#">العنوان :الدوار السابع ، مجمع الشروقي</a>
+              <a target="_blank" rel="noreferrer" href="#">العنوان :الدوار السابع ، مجمع الشروقي</a>
             </li>
 
             <li>
-              <a target="_blank" href="tel:+962-796-868-500">+962-796-868-500</a>
+              <a target="_blank" rel="noreferrer" href="tel:+962-796-868-500">+962-796-868-500</a>
             </li>
           </ul>
         </div>
